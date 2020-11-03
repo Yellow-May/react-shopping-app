73f7b9f62db633d72a2219e8a4cae410c91c42c8
Yellow May <kwerep09@yahoo.com>
2020-11-03 09:38:24 +0100

    shop 2.0

---
+++ src/components/Data/Item.js
@@ -0,0 +1,12 @@
+class Item {
+  constructor(id, name, price, total, quantity, url) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.total = total;
+    this.quantity = quantity;
+    this.url = url;
+  }
+}
+
+export default Item;
